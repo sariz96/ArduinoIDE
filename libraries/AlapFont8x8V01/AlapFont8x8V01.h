2e7c2a9f22075fa70f1b0359a29469cdc6678ad8
Zoltan Sari <sari.liala@gmail.com>
2016-12-16 21:03:14 +0100

    First 8x8 font
    First 8x8 font

---
+++ libraries/AlapFont8x8V01/AlapFont8x8V01.h
@@ -0,0 +1,37 @@
+﻿#ifndef AlapFont8x8V01_h
+#define AlapFont8x8V01_h
+/* -- font class for LED matrix
+   -- Library was created: 2016.11.19 01:38:02.477
+   -- Zoltan Sari
+*/
+
+#ifdef __AVR__
+ #include <avr/io.h>
+ #include <avr/pgmspace.h>
+#elif defined(ESP8266)
+ #include <pgmspace.h>
+#else
+ #define PROGMEM
+#endif
+
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include "ZMaxLEDFont.h"
+
+class AlapFont8x8V01 : public ZMaxLEDFontBase
+{
+ public:
+   AlapFont8x8V01();
+
+ protected:
+    static const uint16_t v_charInfoVector[];
+    static const uint8_t v_charTable[];
+
+};
+#endif
+
