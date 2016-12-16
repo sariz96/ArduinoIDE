75ac2c68cc1a40330696fd6abf036800b209c98e
Zoltan Sari <sari.liala@gmail.com>
2016-12-16 03:20:25 +0100

    private settings
    private settings

--- libraries/ZWiFiConfig/ZWiFiConfig.cpp
+++ libraries/ZWiFiConfig/ZWiFiConfig.cpp
@@ -9,18 +9,18 @@
 
  /* --- WiFi beállítások -- */
 const char ZWiFiConfig_ssid_01[] = "Z42HomePriv";
-const char ZWiFiConfig_password_01[] = "Go9753zos";
+const char ZWiFiConfig_password_01[] = "xxxxxxxxxx";
 
 const char ZWiFiConfig_ssid_02[] = "T-76B490";
-const char ZWiFiConfig_password_02[] = "qX7WY7hY";
+const char ZWiFiConfig_password_02[] = "xxxxxxxxxxxxx";
 
 const char ZWiFiConfig_ssid_03[] = "Boldizsar24";
-const char ZWiFiConfig_password_03[] = "B0ld1z$@R24!";
+const char ZWiFiConfig_password_03[] = "xxxxxxxx";
 
 const char *ZWiFiConfig_ssid[] = {ZWiFiConfig_ssid_01, ZWiFiConfig_ssid_02, ZWiFiConfig_ssid_03};
 const char *ZWiFiConfig_password[] = {ZWiFiConfig_password_01, ZWiFiConfig_password_02, ZWiFiConfig_password_03};
 
  /* --- AP beállítások -- */
 const char ZWiFiConfig_ssid_AP_Prefix[] = "ESP_";
-const char ZWiFiConfig_AP_password_01[] = "WelCome$1208";
+const char ZWiFiConfig_AP_password_01[] = "xxxxxxxxxxxx";
 
